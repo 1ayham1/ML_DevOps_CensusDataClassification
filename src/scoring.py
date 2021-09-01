b7b68e57182a1558d458e0206ebdc9a604e9ba7c
me <learn.ayham@zoho.com>
2021-09-01 09:49:16 +0200

    restructuring... testing basic training and scoring

---
+++ src/scoring.py
@@ -0,0 +1,78 @@
+"""Calculating F1-Score of a trained model"""
+
+import pickle
+import os
+import logging
+
+from sklearn.metrics import fbeta_score, precision_score, recall_score
+
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
+logger = logging.getLogger()
+
+
+model_folder = os.path.abspath('model')
+
+
+def score_model(X_train,X_test,y_train,y_test):
+    """Function for model scoring
+
+    Given a trained model, relevent data is loaded, and F1 score is
+    then calculated. 
+
+    """
+
+    logger.info("read and load trained model")
+
+    model_path = os.path.join(model_folder, "trainedmodel.pkl")
+    with open(model_path, 'rb') as file:
+        model = pickle.load(file)
+
+
+    logger.info("Scoring data_train: new data is ingested.")
+    
+    preds_train = model.predict(X_train)
+    precision_tr, recall_tr, fbeta_tr = compute_model_metrics(y_train, preds_train)
+  
+    print(
+    f"""Training scores....\n{'-'*50}\n
+    precision: {precision_tr}\n
+    recall: {recall_tr}\n
+    fbeta: {fbeta_tr}\n""")
+
+    print(f"{'='*50}")
+
+    logger.info("Scoring data_test")
+
+    preds_test = model.predict(X_test)
+    precision_test, recall_test, fbeta_test = compute_model_metrics(y_test, preds_test)
+    print(
+        f"""Test scores....\n{'-'*50}\n
+        precision: {precision_test}\n
+        recall: {recall_test}\n
+        fbeta: {fbeta_test}\n""")
+
+
+def compute_model_metrics(y, preds):
+    """
+    Validates the trained machine learning model using precision, recall, and F1.
+
+    Inputs
+    ------
+    y : np.array
+        Known labels, binarized.
+    preds : np.array
+        Predicted labels, binarized.
+    Returns
+    -------
+    precision : float
+    recall : float
+    fbeta : float
+    """
+    fbeta = fbeta_score(y, preds, beta=1, zero_division=1)
+    precision = precision_score(y, preds, zero_division=1)
+    recall = recall_score(y, preds, zero_division=1)
+    
+    #accuracy = sum(y == preds) / float(len(preds))
+
+    return precision, recall, fbeta
---
+++ src/train_model.py
@@ -0,0 +1,123 @@
+"""Script to train machine learning model.
+
+ predict person's income [>50k or <50k] using various features
+ {age, education, marital-status, relatoinship, race, sex}
+"""
+
+import os
+import pickle
+
+import logging
+import joblib
+
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OrdinalEncoder, OneHotEncoder, FunctionTransformer,StandardScaler
+from sklearn.pipeline import Pipeline, make_pipeline
+from sklearn.impute import SimpleImputer
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+from sklearn.metrics import f1_score
+
+import scoring
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
+logger = logging.getLogger()
+
+model_path = os.path.abspath('model')
+model_name = os.path.join(model_path, "trainedmodel.pkl")
+
+def data_split():
+    """returns data {X,y} splitted in training and testing"""
+    
+    df_data = pd.read_csv("../data/clean_census_data.csv")
+
+
+    X_data = df_data.drop('salary', axis=1)
+    y_response = df_data['salary']
+
+    X_train, X_test, y_train, y_test = train_test_split(X_data, y_response, 
+                                                        test_size = 50,
+                                                        random_state = 2,
+                                                        stratify = y_response)
+
+    return X_train, X_test, y_train, y_test
+
+
+ 
+def get_inference_pipeline(clf_model):
+    """classify and preprocess data. return inference pipeline"""
+    
+    ordinal_categorical = ["education","occupation"]
+    
+    ordinal_categorical_preproc = make_pipeline(
+        SimpleImputer(strategy="most_frequent"), 
+        OrdinalEncoder(),
+    )
+
+    non_ordinal_categorical = ["sex","marital-status","relationship","race","native-country"]
+    
+    non_ordinal_categorical_preproc = make_pipeline(
+        SimpleImputer(strategy="most_frequent"), 
+        OneHotEncoder(),
+    )
+
+    numeric_cols = ["age", "hours-per-week"]
+    numeric_cols_preproc = make_pipeline(
+        SimpleImputer(strategy="median"), StandardScaler()
+    )
+    
+    #put everything together
+    preprocessor = ColumnTransformer(
+        transformers=[
+            ("ordinal_cat", ordinal_categorical_preproc, ordinal_categorical),
+            ("non_ordinal_cat", non_ordinal_categorical_preproc, non_ordinal_categorical),
+            ("impute_numer", numeric_cols_preproc, numeric_cols),
+        ],
+        remainder="drop",  # This drops the columns that we do not transform
+    )
+
+    
+    #Use the explicit Pipeline constructor so you can assign the names to the steps, do not use make_pipeline
+    sk_pipe = Pipeline([
+            ("preprocessor", preprocessor),
+            ("random_forest", clf_model),
+    ])
+
+    return sk_pipe
+
+
+def train_models(train_from_scratch=True):
+    '''Run model inferences and print out scores
+    
+    train_from_scratch: True to train a model
+    '''
+    
+    logger.info("reading and processing data")
+    X_train, X_test, y_train, y_test = data_split()
+
+    if(train_from_scratch):
+
+        logger.info("Training a new model............")
+        clf_svm = SVC(random_state = 42, kernel='rbf')
+        
+        model = get_inference_pipeline(clf_svm)
+        
+        logger.info("fitting model..........")
+        model.fit(X_train, y_train)
+
+        logger.info("saving the model............")
+        pickle.dump(model, open(model_name, 'wb'))
+
+    else:
+        logger.info("loading saved model............")
+        model = joblib.load(model_name)
+
+    logger.info("Model Evaluation............\n")
+    scoring.score_model(X_train, X_test, y_train, y_test)
+    
+
+if __name__ == "__main__":
+    train_models(train_from_scratch=True)
