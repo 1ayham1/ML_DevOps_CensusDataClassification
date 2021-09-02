2edfb5801d2a766f77b7d38f1998ff6e966149ef
me <learn.ayham@zoho.com>
2021-09-02 10:22:08 +0200

    adjusting test size

--- src/train_model.py
+++ src/train_model.py
@@ -40,8 +40,8 @@ def data_split():
     y_response = df_data['salary']
 
     X_train, X_test, y_train, y_test = train_test_split(X_data, y_response, 
-                                                        test_size = 50,
-                                                        random_state = 2,
+                                                        test_size = 0.2,
+                                                        random_state = 42,
                                                         stratify = y_response)
 
     return X_train, X_test, y_train, y_test
